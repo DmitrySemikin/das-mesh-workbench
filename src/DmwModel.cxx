ea1c1aa0a4a646ae4758e90194df7dee68b409e7
Dmitrii Semikin <dmitry.semikin@gmail.com>
2020-12-14 22:34:44 +0100

    Start factoring out Model and Viewer. Still work in progress

--- src/DmwModel.cxx
+++ src/DmwModel.cxx
@@ -1,7 +1,40 @@
+#include <vtkNew.h>
+#include <vtkVectorText.h>
+#include <vtkElevationFilter.h>
+#include <vtkPolyDataMapper.h>
+
+
 #include "DmwModel.hxx"
 
 DmwModel::DmwModel() {
-    // Nothing to do yet
+    // TODO: how do we keep reference to all the pipeline components?
+    // In fact for the meshes themselves we don't really need to keep
+    // objects, which produce UnstructuredMesh. We need to only keep
+    // the mesh itself (we may though store the parameters, like
+    // file name of the file, from which it was loaded or parameters
+    // of the gemetric primitive - like cone - from which it was
+    // created, - to enable recreation with another parameters).
+
+    // TODO: Probably to make other discussed points more clear I should switch from using 3dText to using UnstructuredMesh (just create couple of triangles or tetrahedrons).
+
+    // TODO: creation should happen in some other component
+
+    // TODO: we need API to add and remove (delete) visualisable objects
+    // (or actually meshes).
+
+    vtkNew<vtkVectorText> vectorText;
+    vectorText->SetText("DAS-Mesh-Workbench");
+
+    vtkNew<vtkElevationFilter> elevationFilter;
+    elevationFilter->SetInputConnection(vectorText->GetOutputPort());
+    elevationFilter->SetLowPoint(0, 0, 0);
+    elevationFilter->SetHighPoint(10, 0, 0);
+
+    // TODO: Where should mapper and actor go? Probably I will understand it, when I start implementing visualization options.
+
+    // TODO: For now add here PolyDataMapper and Actor from DmwMainWindows.cxx
+
+    // Add GUI for creating and deleting objects.
 }
 
 DmwModel::~DmwModel() {
--- src/DmwViewer.cxx
+++ src/DmwViewer.cxx
@@ -1,7 +1,10 @@
 #include "DmwViewer.hxx"
 
 DmwViewer::DmwViewer() {
-    // Nothing to do yet.
+    // TODO: For now add here code from DmwMainWindow to create renderer and renderWindow
+    // TODO: Then connect it with model and with main window
+    // TODO: Then add api to change visualisation modes: shaded, wireframe, both.
+    // TODO: Provide GUI fro switching viewer modes.
 }
 
 DmwViewer::~DmwViewer() {
